48c1c39679b020cf1fdf8f29a1d614a75f61354b
Codadrien <adri1gonon@gmail.com>
2023-02-03 07:25:19 +0100

    [Étape 6]  Insérer un produit et ses détails dans la page Produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,20 @@
+// On récupére l’ id du produit à afficher
 const str = window.location.href;
 const url = new URL(str);
 const id = url.searchParams.get("id");
-console.log("Id =", id);
+
+//On requête l'api
+fetch("http://localhost:3000/api/products/" + id)
+    .then((response) => {
+        return response.json();
+    })
+
+    .then((kanap) => {
+        // On insérer un produit et ses détails dans la page Produit
+        document.getElementById("headTitle").textContent = kanap.name;
+        document.getElementById("img-produits").src = kanap.imageUrl;
+        document.getElementById("img-produits").alt = kanap.altTxt;
+        document.getElementById("title").textContent = kanap.name;
+        document.getElementById("price").textContent = kanap.price;
+        document.getElementById("description").textContent = kanap.description;
+    })
